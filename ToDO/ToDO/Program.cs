3e522cb598fb204a5add635a1100f2c11a8c4406
bkorbuly <b.korbuly@gmail.com>
2017-09-28 10:49:51 +0200

    Print usage feature added

--- ToDO/ToDO/Program.cs
+++ ToDO/ToDO/Program.cs
@@ -10,6 +10,19 @@ namespace ToDO
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            WelcomeMsgWithoutArguments();
+            Console.ReadLine();
+        }
+        static void WelcomeMsgWithoutArguments()
+        {
+            Console.WriteLine("Command Line Todo application");
+            Console.WriteLine("=============================\n");
+            Console.WriteLine("Command Line arguments:");
+            Console.WriteLine("-l List all the task");
+            Console.WriteLine("-a Adds a new task");
+            Console.WriteLine("-r Removes a task");
+            Console.WriteLine("-c Completes a task");
         }
     }
 }
